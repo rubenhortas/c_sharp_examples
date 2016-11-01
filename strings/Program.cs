a2b772572cc9f90279516ad5b89fa685b5f8c747
Rubén <ruben.hortas@gmail.com>
2016-11-01 19:05:34 +0100

    Added string examples

---
+++ strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print a char many times
+            string s1 = new string('*', 25);
+            Console.WriteLine(s1);
+
+            // Interpolation strings
+            string minion = "Mini Me";
+            string evilGenius = "Dr. Evil";
+
+            string s2 = $"{evilGenius} has a minion called {minion}";
+            Console.WriteLine(s2);
+
+            Console.ReadLine();
+        }
+    }
+}
