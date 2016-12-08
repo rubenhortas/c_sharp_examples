9e8afe12b8868caa96aadf01fea1977ce92d08dd
Rubén <ruben.hortas@gmail.com>
2016-12-08 18:31:45 +0100

    Updated

---
+++ ExamQuestions/MultipleInterfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExamQuestions
+{
+    public interface IFile
+    {
+        void Open();
+    }
+
+    public interface IDbConnection
+    {
+        void Open();
+    }
+
+    public class MultipleInterfaces : IFile, IDbConnection
+    {
+        void IDbConnection.Open()
+        {
+            Console.WriteLine("IDbConnection open");
+        }
+
+        void IFile.Open()
+        {
+            Console.WriteLine("IFile open");
+        }
+    }
+}
--- ExamQuestions/Program.cs
+++ ExamQuestions/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ExamQuestions
+﻿using System;
+
+namespace ExamQuestions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            SqlOrderBy.Start();
+            //SqlOrderBy.Start();
+
+            var multipleInterfaces = new MultipleInterfaces();
+            ((IFile)multipleInterfaces).Open();
+            ((IDbConnection)multipleInterfaces).Open();
+            Console.ReadLine();
+
         }
     }
 }
