c46258d953502550ab46f6f89cd4b1c2aebb59c7
Rubén <ruben.hortas@gmail.com>
2017-03-19 17:55:01 +0100

    Added date formatting

---
+++ DateFormatting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DateFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature.DisplayTemperature();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DateFormatting/Temperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace DateFormatting
+{
+    public static class Temperature
+    {
+        public static void DisplayTemperature()
+        {
+            //modify the vlaue of en-us to Culture.
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-us");
+            Console.WriteLine(string.Format("Temperature at {0:t} on {0:d} {1:N2}",DateTime.Now, 25.45678));
+        }
+    }
+}
