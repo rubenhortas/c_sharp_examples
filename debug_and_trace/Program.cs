8a44ec71f91090a67b111600badf69f654cd3239
Rubén <ruben.hortas@gmail.com>
2017-04-08 14:06:25 +0200

    Added log usage examples

---
+++ debug_and_trace/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace debug_and_trace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriterTraceListener tr1 = new TextWriterTraceListener(Console.Out);
+            Debug.Listeners.Add(tr1);
+
+            TextWriterTraceListener tr2 = new TextWriterTraceListener(System.IO.File.CreateText("Output.txt"));
+            Debug.Listeners.Add(tr2);
+
+            LogDebug(); // only in debug mode
+            LogTrace(); // debug and release mode
+
+            Console.ReadLine();
+        }
+
+        private static void LogDebug()
+        {
+            bool showAssert = true;
+
+            Debug.WriteLine("Debug line");
+            Debug.Indent();
+            Debug.WriteLineIf(showAssert == true, "showAssert is true");
+            //Debug.Assert(showAssert == true, "assert message");
+            //Debug.Assert(showAssert == false, "no message beacuse showAssert is false");
+            Debug.Unindent();
+
+            Debug.Flush();
+        }
+
+        private static void LogTrace()
+        {
+            bool showAssert = true;
+
+            Trace.WriteLine("Trace line");
+            Trace.Indent();
+            Trace.WriteLineIf(showAssert == true, "showAssert is true");
+            //Trace.Assert(showAssert == true, "assert message");
+            //Trace.Assert(showAssert == false, "no message because showAssert is false");
+            Trace.Unindent();
+
+            Trace.Flush();
+        }
+    }
+}
---
+++ log4net_example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace log4net_example
+{
+    /**
+     * Steps:
+     *  1) Add [assembly: log4net.Config.XmlConfigurator(Watch=true)] to AssemblyInfo.cs
+     *      This way log4net will know that it can look for configuration in the default application config file
+     *  2) Add <section name="log4net" type="log4net.Config.Log4NetConfigurationSectionHandler, log4net"/> to App.config
+     *      This time log4net will know that the real configuration is stored in XML node called log4net
+     *  3) Add section log4net to App.config
+     *  4) Configure logging level in App.config
+     *  5) Configure appender[s] and add the appender[s] section[s]
+    **/
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("System.Reflection.MethodBase.GetCurrentMethod().DeclaringType: {0}", System.Reflection.MethodBase.GetCurrentMethod().DeclaringType));
+            log.Fatal("Fatal");
+            log.Error("Error");
+            log.Warn("Warn");
+            log.Info("Info");
+            log.Debug("Debug");
+            Console.ReadLine();
+        }
+    }
+}
