be3ab28112e5f2a5d53c03109007b55903690b87
ruben <ruben.hortas@gmail.com>
2020-07-07 23:21:52 +0200

    Added examples

--- strings/Program.cs
+++ strings/Program.cs
@@ -8,14 +8,47 @@ namespace strings
         {
             // Print a char many times
             string s1 = new string('*', 25);
-            Console.WriteLine(s1);
+            Console.WriteLine(s1); // *************************
 
             // Interpolation strings
             string minion = "Mini Me";
             string evilGenius = "Dr. Evil";
 
-            string s2 = $"{evilGenius} has a minion called {minion}";
+            string s2 = $"{evilGenius} has a minion called {minion}"; // Dr. Evil has a minion called Mini Me
             Console.WriteLine(s2);
+
+            // Join
+            // This method is used to concatenates the members of a collection or the elements of the specified array,
+            // using the specified separator between each member or element. 
+            string[] helloWorldArray = { "hello", "world!" };
+
+            string joinedString = String.Join(",", helloWorldArray);
+            Console.WriteLine(joinedString); // hello,world!
+
+            // Split
+            string str = "one, two, three";
+            string[] splittedString = str.Split();
+
+            foreach (string s in splittedString)
+            {
+                Console.WriteLine(s);   //one, 
+                                        // two,
+                                        // three
+            }
+
+            // Contains
+            string numbers = "1234513370000";
+
+            if (numbers.Contains("1337"))
+            {
+                Console.WriteLine("Y0u'r3 1337!"); // Y0u'r3 1337!
+            }
+
+            // Remove
+            // Remove last char
+            string helloWorld = "Hello, world!!";
+            string cleanHelloWorld = helloWorld.Remove(helloWorld.Length - 1);
+            Console.WriteLine(cleanHelloWorld); // Hello, world!
 
             Console.ReadLine();
         }
